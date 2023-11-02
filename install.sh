b17ef5672a65c97049cde810c2af506c1b9781b9
kriss <kriss@xixs.com>
2023-11-02 21:55:31 +0000

    .

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
-GDIR="$HOME/.config/geany"
-echo "Installing themes into \`$GDIR'..."
-mkdir -p "$GDIR/colorschemes/"
-for THEME in `ls colorschemes/*.conf`
-do
-  BNAME=`basename "$THEME"`
-  echo " => $BNAME"
-  cp "$THEME" "$GDIR/colorschemes/"
-done
 
-echo "Replacing geany.conf"
-cp ./geany.conf "$GDIR/"
+echo "Replacing geany.conf and themes"
+cp -r ./geany/*  "$HOME/.config/geany/"
 
