8f858421870f39b8375193a608f49884b10e0b36
kriss <kriss@xixs.com>
2023-11-02 22:29:56 +0000

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 cd `dirname $0`
 
+# first install a require script
+if ! [[ -x "$(command -v require.sh)" ]] ; then
+
+	echo " we need sudo to install require.sh to /usr/local/bin "
+	sudo wget -O /usr/local/bin/require.sh https://raw.githubusercontent.com/xriss/require.sh/main/require.sh
+	sudo chmod +x /usr/local/bin/require.sh
+
+fi
+
+require.sh hunspell-en_us
+
 echo "Replacing geany.conf and themes in $HOME/.config/geany/"
 cp -r ./geany/*  "$HOME/.config/geany/"
 
